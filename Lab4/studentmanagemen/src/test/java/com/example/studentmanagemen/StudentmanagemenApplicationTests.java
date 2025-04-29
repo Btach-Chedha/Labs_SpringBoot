79434d99d84e28ecf160c67d9284b991aef64855
btach_chedha <chedha.bt9@gmail.com>
2025-04-29 23:42:46 +0100

    Lab4

---
+++ Lab4/studentmanagemen/src/test/java/com/example/studentmanagemen/StudentmanagemenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagemen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentmanagemenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
